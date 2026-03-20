e479499a474418c93c36345cb70e31f06dd44824
Josehpequeno <hicarosoares@uespi.br>
2026-03-20 11:14:32 -0300

    ajeitando o cors para todos

--- docs/docs.go
+++ docs/docs.go
@@ -164,7 +164,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:5005",
+	Host:             "",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "API de Geração de Boletos",
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	_ "gre-api/docs" // Substitua "gre-api" pelo nome do seu módulo
 	"gre-api/internal/handlers"
+	"time"
 
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -14,16 +16,30 @@ import (
 // @title API de Geração de Boletos
 // @version 1.0
 // @description API que recebe dados e gera um boleto em HTML.
-// @host localhost:5005
+// @host
 // @BasePath /
 func main() {
 	r := gin.Default()
+
+	// r.Use(cors.Default())
+	//cors para liberar específico
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders: []string{
+			"Content-Length",
+			"Content-Disposition",
+		},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.POST("/boleto", handlers.RenderBoleto)
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	log.Println("Servidor rodando em http://localhost:6000")
+	log.Println("Servidor rodando em http://localhost:5005")
 	if err := r.Run(":5005"); err != nil {
 		log.Fatal(err)
 	}
